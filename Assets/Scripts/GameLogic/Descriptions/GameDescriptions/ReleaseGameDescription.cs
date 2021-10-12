083ed7593841682c963bdfc34148367370c12cc9
Maxim Grazhevsky <m.grazhevsky@kefirgames.com>
2021-10-12 10:59:07 +0300

    add: time session system

--- Assets/Scripts/GameLogic/Descriptions/GameDescriptions/ReleaseGameDescription.cs
+++ Assets/Scripts/GameLogic/Descriptions/GameDescriptions/ReleaseGameDescription.cs
@@ -1,4 +1,5 @@
-﻿using GameLogic.Systems.Spaceship;
+﻿using GameLogic.Systems;
+using GameLogic.Systems.Spaceship;
 using GameLogic.Systems.Spaceship.Movement;
 using Leopotam.Ecs;
 
@@ -6,12 +7,11 @@ namespace GameLogic.Descriptions.GameDescriptions
 {
     public class ReleaseGameDescription : GameDescription
     {
-        protected override void SetupAll(EcsSystems systems)
-        {
-        }
+        protected override void SetupAll(EcsSystems systems) { }
 
         public override void SetupSystems(EcsSystems systems)
         {
+            systems.Add(new TimeSessionSystem());
             systems.Add(new SpaceshipSpawnSystem());
             systems.Add(new SpaceshipVelocitySystem());
             systems.Add(new SpaceshipRotateSystem());
---
+++ Assets/Scripts/GameLogic/Systems/TimeSessionSystem.cs
@@ -0,0 +1,24 @@
+﻿using GameLogic.Components;
+using GameLogic.Dependencies;
+using Leopotam.Ecs;
+
+namespace GameLogic.Systems
+{
+    public class TimeSessionSystem : IEcsInitSystem, IEcsRunSystem
+    {
+        private EcsFilter<TimeSessionComponent> _filter;
+        private EcsWorld _world;
+        private IDeltaTime _deltaTime;
+        
+        public void Init() => _world.NewEntity().Replace(new TimeSessionComponent());
+
+        public void Run()
+        {
+            if (_filter.IsEmpty() == false)
+            {
+                ref var timeSessionComponent = ref _filter.Get1(0);
+                timeSessionComponent.Time += _deltaTime.Value;
+            }
+        }
+    }
+}
